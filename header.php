9ee707121791c3e1bd50567b13cac25209058361
hafiznabil <160453395+hafiznabil@users.noreply.github.com>
2024-09-27 20:52:29 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//extract($POST);
+
+//connect to server
+$connect = mysqli_connect ("localhost", "root", "", "klinik_ajwa");
+
+if(!$connect){
+    die('ERROR:'.mysqli_connect_error());
+}
+
+?>
+
+</body>
+</html>
